f418647aed1b2dd9456b64be1546a7704fc57748
Mark Burns <markthedeveloper@gmail.com>
2013-06-22 09:31:22 +0100

    deleted files as part of reorganizing

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -41,19 +41,19 @@ module Haml
 
     def linkable_word text, word_class=''
       word_class= " class='#{word_class}'" if word_class.present?
-      @query_without_quotes ||=  @query.gsub /"/,""
+      @query_without_quotes ||=  @query.gsub(/"/,"")
       link_full_text = @query_without_quotes == text
 
       if link_full_text
-        return "<a class='highlight' href='/word-search?query=#{@query}'>#{text}</a>"
+        "<a class='highlight' href='/word-search?query=#{@query}'>#{text}</a>"
 
       elsif text =~ /(^|\b)#{@query}\b/
-        before, after = text.split /(^|\b)#{@query}/
+        before, after = text.split(/(^|\b)#{@query}/)
         link = "<span#{word_class}>#{before.try :strip}"
         link << "<a class='highlight' href='/word-search?query=#{@query.strip}'>#{@query_without_quotes}</a>"
         link << "#{after.try :strip}</span>"
       else
-        unquoted = text.gsub /"/,''
+        unquoted = text.gsub(/"/,'')
         "<a href='/word-search?query=#{text}'><span#{word_class}>#{unquoted}</span></a>"
       end
     end
--- spec/tokenizer_spec.rb
+++
@@ -1,6 +0,0 @@
-require File.expand_path('spec/spec_helper')
-require File.expand_path('lib/tokenizer')
-
-describe Tokenizer do
-
-end
