62a738bd9fedd5a1f236a3b4aa0d6e487827d0c6
Mark Burns <markthedeveloper@gmail.com>
2013-07-06 14:50:19 +0100

    move away from cucumber and start using request specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,14 @@
 require './lib/all_requires'
+require './wwwjdic'
+require 'capybara/rspec'
+Capybara.javascript_driver = :webkit
+Capybara.app = Wwwjdic
+
+RSpec.configure do |config|
+  config.order = "random"
+  config.include Capybara::DSL
+
+end
 
 module RedisHelpers
   def env
